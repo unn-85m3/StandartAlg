7bf45e4e127be34c7b4bdc7a86a83d53ce73a4a1
MissisWesker <pushistik_92@inbox.ru>
2014-09-28 21:40:21 +0400

    adding algorithm

---
+++ standart_alg/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace standart_alg
+{
+
+
+
+    class standart_alg
+    {
+        private double h; //шаг
+
+        public double Func(double a, double b)
+        {
+            return (1/((a+b)*a*a));
+        }
+
+        public double H{
+            get {return h;}
+            set { h = value;}
+        }
+
+
+        public double Algorythm(double pin_min, double pin_max, double pout_min, double pout_max, double compr_min, double compr_max)
+        {
+            double k, cost;
+
+            cost = Func(pin_min, pout_min);
+
+            for (double i = pin_min; i <= pin_max; i = i + h)
+                for (double j = pout_min; j <= pout_max; j = j + h)
+                    if (((i / j) <= compr_max) && ((i / j) >= compr_min))
+                    {
+                        /*try
+                        {
+                            k = Func(i, j);
+                        }
+                        catch
+                        {  //я по-прежнему не знаю, что делать с эксепшеном
+                        }*/
+                        k = Func(i, j);
+                        Console.WriteLine("k = {0}, i = {1}, j = {2},", k, i, j);
+                        if (k < cost)
+                            cost = k;
+                    }
+            return cost;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            standart_alg A = new standart_alg();
+            A.H = 1;
+            double pin_min = 1;
+            double pin_max = 4;
+            double pout_min = 1;
+            double pout_max = 4;
+            double compr_min = 0.5;
+            double compr_max = 1;
+
+            double res = A.Algorythm(pin_min, pin_max, pout_min, pout_max, compr_min, compr_max);
+            Console.WriteLine(res);
+            Console.Write(A.H);
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ standart_alg/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("standart_alg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("standart_alg")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9bc2391d-8919-42a8-adc2-d6558688add3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
